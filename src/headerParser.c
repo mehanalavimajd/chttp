202010eed4d1c858f036f5a91c9ebb4488cddfd7
mehan <mehan.alavi.88.majd@gmail.com>
2025-07-12 19:26:50 +0330

    feat: headerParser.c can now parse

--- src/headerParser.c
+++ src/headerParser.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define DEFAULT_HEADER_COUNT 30
+#define MAX_HEADER_COUNT 1000
 struct header
 {
 	char *headerName;
@@ -6,25 +10,60 @@ struct header
 typedef struct header Header;
 void headerParser(char *buff)
 {
+	int headerCount = DEFAULT_HEADER_COUNT;
+	Header *headers = malloc(headerCount * sizeof(Header));
 	int i = 0;
 	while (buff[i++] != '\n')
 		; // to pass first line
-	Header currHeader;
-	currHeader.headerName = malloc(1000);
-	currHeader.headerValue = malloc(1000);
-	int pos = 0;
-	while (buff[i] != ':') // ++i passes to first char
+	int currHeaderPos = 0;
+	while (1)
 	{
-		currHeader.headerName[pos++] = buff[i++];
+		if (currHeaderPos >= headerCount)
+		{
+			// printf("mama imma realloc!"); -- still works better that GDB
+			headers = realloc(headers, sizeof(Header) * (headerCount + DEFAULT_HEADER_COUNT));
+			headerCount += DEFAULT_HEADER_COUNT;
+		}
+
+		Header currHeader;
+		currHeader.headerName = malloc(1000);
+		currHeader.headerValue = malloc(1000);
+		int pos = 0;
+		while (buff[i] != ':') // ++i passes to first char
+		{
+			currHeader.headerName[pos++] = buff[i++];
+		}
+		i += 2; // pass space and :
+		pos = 0;
+		while (buff[i] != '\r') // ++i passes to first char
+		{
+			currHeader.headerValue[pos++] = buff[i++];
+		}
+		printf("%s,%s\n", currHeader.headerName, currHeader.headerValue);
+		headers[currHeaderPos++] = currHeader;
+		i += 2;
+		if (buff[i] == '\r')
+		{
+			printf("%d", currHeaderPos);
+			break;
+		}
 	}
-	i += 2; // pass space and :
-	pos = 0;
-	while (buff[i] != '\n') // ++i passes to first char
+	for (int i = 0; i < currHeaderPos; i++)
 	{
-		currHeader.headerValue[pos++] = buff[i++];
+		free(headers[i].headerName);
+		free(headers[i].headerValue);
 	}
-	printf("%s,%s\n", currHeader.headerName, currHeader.headerValue);
+
+	free(headers);
 }
-int main(){
-	headerParser("GET /r HTTP1.1\r\nhello: world\r\n");
+int main()
+{
+	headerParser(
+	    "GET /r HTTP1.1\r\n"
+	    "hello: world\r\n"
+	    "life: good\r\n"
+	    "life: good\r\n"
+	    "life: good\r\n"
+	    "life: good\r\n"
+	    "\r\n");
 }
